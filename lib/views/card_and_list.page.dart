87f49f6ce8d866c4c20ed9fad0ee7f2a9b981ac2
Philippe Plaïa <philippeplaia@gmail.com>
2025-04-15 16:34:29 +0200

    card and title

--- lib/views/card_and_list.page.dart
+++ lib/views/card_and_list.page.dart
@@ -5,9 +5,27 @@ class CardsAndList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Cards and List')),
-      body: Center(
-        child: Text('Cards and List'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Cards and List')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Cards and List'),
+          Card(
+            elevation: 4.0,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Titre de la carte', style: TextStyle(fontWeight: FontWeight.bold)),
+                  SizedBox(height: 8.0), // Ajout d'un espace entre les textes
+                  Text('Contenu de la carte'),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
