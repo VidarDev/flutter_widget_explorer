0ddd0808d750fd86538385fb1004b547013f5483
Matthys Boureau <145988911+MatthysBoureau@users.noreply.github.com>
2025-04-16 11:20:38 +0200

    [🚀 - Feature] - MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:flutter_widget_explorer/views/scaffold_appbar.page.dart';
 import 'package:flutter_widget_explorer/views/scollviews_slivers.page.dart';
 import 'package:flutter_widget_explorer/views/search_bar.page.dart';
 import 'package:flutter_widget_explorer/views/stack_position.page.dart';
+import 'package:flutter_widget_explorer/views/media_query.page.dart';
 
 // At the top of your HomePage class
 final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -325,7 +326,7 @@ class HomePage extends StatelessWidget {
                 Navigator.pop(context);
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => Container()),
+                  MaterialPageRoute(builder: (context) => MediaQueryViewPage()),
                 );
               },
             ),
